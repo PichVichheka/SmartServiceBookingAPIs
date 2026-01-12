3ea4cccd900109db30357d24f1c38855010f1eb4
PichVichheka <pich.vicheka@institute.pse.ngo>
2026-01-12 16:14:31 +0700

    first commit

---
+++ src/main/java/com/smartService/SmartServiceBookingAPIs/SmartServiceBookingApIsApplication.java
@@ -0,0 +1,14 @@
+package com.smartService.SmartServiceBookingAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartServiceBookingApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartServiceBookingApIsApplication.class, args);
+        System.out.println("Application is running!");
+	}
+
+}
