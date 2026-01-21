38d91b9d28d0aba79664dab04dad6b5bf8cde131
PichVichheka <pich.vicheka@institute.pse.ngo>
2026-01-21 15:43:33 +0700

    add service provider

--- src/main/java/com/smartService/SmartServiceBookingAPIs/Repositories/ServiceRepository.java
+++ src/main/java/com/smartService/SmartServiceBookingAPIs/Repositories/ServiceRepository.java
@@ -1,6 +1,13 @@
 package com.smartService.SmartServiceBookingAPIs.Repositories;
 
 import com.smartService.SmartServiceBookingAPIs.Entity.Services;
+import com.smartService.SmartServiceBookingAPIs.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServiceRepository extends JpaRepository<Services, Long> {}
+import java.util.List;
+
+public interface ServiceRepository extends JpaRepository<Services, Integer> {
+
+    List<Services> findByProvider(Users provider);
+
+}
